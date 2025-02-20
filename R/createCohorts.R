4ecd88e4f52a5f56a7e0249cea1970f5553986ed
Admin_FBu2 <Admin_FBu2@its.jnj.com>
2025-02-19 21:41:54 -0500

    minor bugfix on cohort creation functions

--- R/createCohorts.R
+++ R/createCohorts.R
@@ -4,19 +4,24 @@ createCohortsFromFiles <- function(connectionDetails,
                                    cohortDatabaseSchema,
                                    cohortTableName,
                                    cohortFileList,
-                                   outputPath){
+                                   outputPath,
+                                   indexPrefix = 100){
+  
+  mes = paste("Creating cohorts in table", cohortTableName)
+  ParallelLogger::logInfo(mes)
+  
   cohortsToCreate <- CohortGenerator::createEmptyCohortDefinitionSet()
   
-  for(i in 1:length(cohortFileList)){
+  for(i in c(1:length(cohortFileList))){
     cohortJsonFileName <- cohortFileList[i]
     cohortName <- tools::file_path_sans_ext(basename(cohortJsonFileName))
     cohortJson <- readChar(cohortJsonFileName, file.info(cohortJsonFileName)$size)
     cohortExpression <- CirceR::cohortExpressionFromJson(cohortJson)
-    cohortSql <- CirceR::buildCohortQuery(cohortExpression, 
+    cohortSql <- CirceR::buildCohortQuery(cohortExpression,
                                           options = CirceR::createGenerateOptions(generateStats = FALSE))
     cohortsToCreate <- rbind(cohortsToCreate,
-                             data.frame(cohortId = i,
-                                        cohortName = cohortName, 
+                             data.frame(cohortId = i+indexPrefix,
+                                        cohortName = cohortName,
                                         sql = cohortSql,
                                         stringsAsFactors = FALSE))
     
@@ -28,6 +33,9 @@ createCohortsFromFiles <- function(connectionDetails,
   CohortGenerator::createCohortTables(connectionDetails = connectionDetails,
                                       cohortDatabaseSchema = cohortDatabaseSchema,
                                       cohortTableNames = cohortTableNames)
+  
+  ParallelLogger::logInfo("cohort table created.")
+  
   # Generate the cohorts
   cohortsGenerated <- CohortGenerator::generateCohortSet(connectionDetails = connectionDetails,
                                                          cdmDatabaseSchema = cdmDatabaseSchema,
@@ -35,9 +43,56 @@ createCohortsFromFiles <- function(connectionDetails,
                                                          cohortTableNames = cohortTableNames,
                                                          cohortDefinitionSet = cohortsToCreate)
   # Get cohort counts
+  ParallelLogger::logInfo("getting cohort counts")
   cohortCounts <- CohortGenerator::getCohortCounts(connectionDetails = connectionDetails,
                                                    cohortDatabaseSchema = cohortDatabaseSchema,
                                                    cohortTable = cohortTableNames$cohortTable)
+  countsFileName = paste0(cohortTableName, "_cohortCounts.csv")
+  readr::write_csv(cohortCounts, file.path(outputPath, countsFileName))
+}
+
+createNegativeControlsCohorts <- function(connectionDetails,
+                                          cdmDatabaseSchema,
+                                          cohortDatabaseSchema,
+                                          cohortTableName,
+                                          negativeControlsCsv,
+                                          outputPath,
+                                          oracleTempSchema = NULL){
+  
+  connection <- DatabaseConnector::connect(connectionDetails)
+  on.exit(DatabaseConnector::disconnect(connection))
+  
+  ParallelLogger::logInfo("Creating negative control outcome cohorts")
+  negativeControls <- read.csv(negativeControlsCsv)
+  data <- negativeControls[, c("conceptId", "cohortId")]
+  colnames(data) <- SqlRender::camelCaseToSnakeCase(colnames(data))
+  DatabaseConnector::insertTable(connection = connection,
+                                 tableName = "#negative_controls",
+                                 data = data,
+                                 dropTableIfExists = TRUE,
+                                 createTable = TRUE,
+                                 tempTable = TRUE,
+                                 oracleTempSchema = oracleTempSchema)
+  sql <- SqlRender::loadRenderTranslateSql("NegativeControls.sql",
+                                           "PCVDosingSchedule",
+                                           dbms = connectionDetails$dbms,
+                                           oracleTempSchema = oracleTempSchema,
+                                           cdm_database_schema = cdmDatabaseSchema,
+                                           target_database_schema = cohortDatabaseSchema,
+                                           target_cohort_table = cohortTableName)
+  DatabaseConnector::executeSql(connection, sql)
+  
+  sql <- "TRUNCATE TABLE #negative_controls; DROP TABLE #negative_controls;"
+  sql <- SqlRender::translate(sql = sql,
+                              targetDialect = connectionDetails$dbms,
+                              oracleTempSchema = oracleTempSchema)
+  DatabaseConnector::executeSql(connection, sql, progressBar = FALSE, 
+                                reportOverallTime = FALSE)
+  
+  # get counts 
+  cohortCounts <- CohortGenerator::getCohortCounts(connectionDetails = connectionDetails,
+                                                   cohortDatabaseSchema = cohortDatabaseSchema,
+                                                   cohortTable = cohortTableName)
   countsFileName = paste0(cohortTableName, "_cohortCounts.csv")
   readr::write_csv(cohortCounts, file.path(outputPath, countsFileName))
 }
